e59dfa94e58a56701b82375ef9a25c97e2626b48
alva <zwei22@wustl.edu>
2020-01-08 18:27:25 -0600

    front

---
+++ src/main/resources/static/precache-manifest.e5e3382950acccd7370ac7d499c776cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aaf2a1b1f9020136ba7d4a0ce9b2e30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ab0ff4bcc3569bbdaec",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "d1c9ab88222bfabee2f2",
+    "url": "/static/css/main.d3c1742d.chunk.css"
+  },
+  {
+    "revision": "5ab0ff4bcc3569bbdaec",
+    "url": "/static/js/2.65c03000.chunk.js"
+  },
+  {
+    "revision": "87984cad984dae3222b2a1d0861003a4",
+    "url": "/static/js/2.65c03000.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d1c9ab88222bfabee2f2",
+    "url": "/static/js/main.7aca8945.chunk.js"
+  },
+  {
+    "revision": "0e0de284a6b544b8ab0b",
+    "url": "/static/js/runtime-main.df9c3e05.js"
+  }
+]);
